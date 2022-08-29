3c2c6f3a33cb66f94ef13e8c62f6fff921ebb617
pinky-banik <pinky.rani.banik@gmail.com>
2022-08-30 03:16:28 +0600

    file upload on server

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const authRoute = require("./routes/auth") ;
-const connectDB = require('./configs/connectdb');
-
+const userRoute = require("./routes/users") ;
+const postRoute = require("./routes/posts");
+const categoryRoute = require("./routes/categories");
+const multer = require("multer");
 
 dotenv.config();
 const PORT = process.env.PORT || 4000;
@@ -19,7 +21,25 @@ mongoose.connect(uri,{
 .then(console.log("connected to mongodb"))
 .catch((err)=>console.log(err));
 
+const storage = multer.diskStorage({
+    destination:(req,file,cb)=>{
+        cb(null,"images")
+    },
+    filename:(req,file,cb)=>{
+        cb(null,req.body.name);
+    }
+});
+
+const upload = multer({storage:storage});
+app.post("/api/upload",upload.single("file"),(req,res)=>{
+    res.status(200).json("File has been uploaded");
+})
+
 app.use("/api/auth", authRoute);
+app.use("/api/users",userRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/categories", categoryRoute);
+
 
 app.use("/",(req,res)=>{
     res.send("Creative talks server");
