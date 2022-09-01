5f195695a34155efd2b13c8a539a0ab9e529d769
pinky-banik <pinky.rani.banik@gmail.com>
2022-09-02 02:04:07 +0600

    server

--- index.js
+++ index.js
@@ -7,14 +7,14 @@ const authRoute = require("./routes/auth") ;
 const userRoute = require("./routes/users") ;
 const postRoute = require("./routes/posts");
 const categoryRoute = require("./routes/categories");
-const multer = require("multer");
+// const multer = require("multer");
 const path = require("path");
 
 dotenv.config();
 const PORT = process.env.PORT || 4000;
 app.use(cors());
 app.use(express.json());
-app.use("/images",express.static(path.join(__dirname,"/images")));
+// app.use("/images",express.static(path.join(__dirname,"/images")));
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5f7tq.mongodb.net/Blog?retryWrites=true&w=majority`;
 
@@ -26,19 +26,19 @@ mongoose.connect(uri,{
 .then(console.log("connected to mongodb"))
 .catch((err)=>console.log(err));
 
-const storage = multer.diskStorage({
-    destination:(req,file,cb)=>{
-        cb(null,"images")
-    },
-    filename:(req,file,cb)=>{
-        cb(null,req.body.name);
-    }
-});
+// const storage = multer.diskStorage({
+//     destination:(req,file,cb)=>{
+//         cb(null,"images")
+//     },
+//     filename:(req,file,cb)=>{
+//         cb(null,req.body.name);
+//     }
+// });
 
-const upload = multer({storage:storage});
-app.post("/api/upload",upload.single("file"),(req,res)=>{
-    res.status(200).json("File has been uploaded");
-})
+// const upload = multer({storage:storage});
+// app.post("/api/upload",upload.single("file"),(req,res)=>{
+//     res.status(200).json("File has been uploaded");
+// })
 
 app.use("/api/auth", authRoute);
 app.use("/api/users",userRoute);
@@ -50,7 +50,7 @@ app.use("/",(req,res)=>{
     res.send("Creative talks server");
 })
 app.get("/", (req, res) => {
-    res.send("Authzee Server Is Running");
+    res.send("Auth Server Is Running");
   });
 app.listen(PORT,()=>{
     console.log("Backend is connected");
